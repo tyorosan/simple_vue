0dca54bb02a4b2b850de8cb77038f028be63a110
tyorosan <tyorosan@gmail.com>
2023-01-11 17:52:28 +0900

    リストへの追加処理を追加

--- ToDoList/scripts/todo.js
+++ ToDoList/scripts/todo.js
@@ -2,6 +2,12 @@ new Vue({
     el: "#app",
     data: {
         todo: "",
-        list: ["hoge", "fuga"],
+        list: ["hoge", "fuga"], //確認用データ
+    },
+    methods: {
+        sendTodo: function () {
+            this.list.push(this.todo);
+            this.todo = "";
+        }
     },
 })
