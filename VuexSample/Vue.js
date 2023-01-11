5137f694406ec29f163ace2fec75182bdabf1db3
Hiroyuki Mori <tyorosan@gmail.com>
2023-01-12 04:41:25 +0900

    Vuexを途中まで実走

---
+++ VuexSample/Vue.js
@@ -0,0 +1,43 @@
+import { Vue } from "vue"
+import { Vuex } from "vuex"
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+  },
+  actions :{
+    increment (context) {
+      context.commit("increment")
+    },
+    decrement (context) {
+      context.commit("decrement")
+    }
+  }
+});
+
+new Vue({
+  el: "#app",
+  computed: {
+    count() {
+      return store.state.count;
+    }
+  },
+  methods: {
+    increment () {
+      store.dispatch("increment");
+    },
+    decrement () {
+      store.dispatch("decrement");
+    },
+  },
+})
